c439b4f14029d7f669a4baa35ebdb0fc3e901ca5
Noer Paanakker <noer@lightworksweb.nl>
2022-01-01 20:41:33 +0100

    created basic schemas

---
+++ backend/schemas/save.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'save',
+  title: 'Save',
+  type: 'document',
+  fields: [
+    { name: 'postedBy', title: 'Posted By', type: 'postedBy' },
+    { name: 'userId', title: 'User ID', type: 'string' },
+  ],
+};
