7c359c7152d4c6f6ee2e09aba6bf30da7b698dfe
Noer Paanakker <noer@lightworksweb.nl>
2022-01-01 21:18:55 +0100

    added core dependencies frontend

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Login: NextPage = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Hello there Login!</h1>
+    </main>
+  );
+};
+
+export default Login;
